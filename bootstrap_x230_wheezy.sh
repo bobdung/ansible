95734d98eb10751935175bbe76e7f418dabfe486
Bob Dung <innerma@gmail.com>
2014-02-23 22:35:04 +0000

    boot strap script modified

--- bootstrap_x230_wheezy.sh
+++ bootstrap_x230_wheezy.sh
@@ -2,6 +2,8 @@
 
 # this is a crude script to get ansible installed on debian wheezy, without
 # using jessie repos, and without trashing the rest of my system. 
+# it then goes on to run a play book which installs some firmware
+# than enables linux to use the wireless card on my Lenovo x230.
 
 apt-get -y install cdbs debhelper dpkg-dev git-core reprepro python-apt
 apt-get -y install python-jinja2 python-yaml python-paramiko sshpass
